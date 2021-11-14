9d34f79cf50bd4faeaf0a98a9f46622b7a052d09
ihipop <ihipop@users.noreply.github.com>
2021-11-14 21:56:04 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@ sudo apt install gcc
 BIN=udpxy
 mkdir -p target
 make clean
-make LDFLAGS=-static release && strip ./${BIN} && mv  ./${BIN} target/${BIN}.`gcc -dumpmachine` && \
+abi=`gcc -dumpmachine`
+make LDFLAGS=-static release && strip ./${BIN} && tar zcvf ${BIN}_${abi}.tar.gz ${BIN} && mv ${BIN}_${abi}.tar.gz target/ && \
 for abi in aarch64-linux-gnu arm-linux-gnueabihf mips-linux-gnu mips64-linux-gnuabi64 mipsel-linux-gnu mips64el-linux-gnuabi64;do
     sudo apt install gcc-${abi} -y && \
     CC=${abi}-gcc && \
@@ -12,6 +13,7 @@ for abi in aarch64-linux-gnu arm-linux-gnueabihf mips-linux-gnu mips64-linux-gnu
     make LDFLAGS=-static  CC=${CC} release && \
     $STRIP ./${BIN} && \
     file ./${BIN} && \
-    mv ./${BIN} ./target/${BIN}.${abi}
+    mkdir -p target/${BIN}_${abi}/ && cp -f ${BIN} target/${BIN}_${abi}/${BIN} && \
+    tar zcvf ${BIN}_${abi}.tar.gz ${BIN} && mv ${BIN}_${abi}.tar.gz target/
 done
-file target/${BIN}*
+file target/*/udpxy
